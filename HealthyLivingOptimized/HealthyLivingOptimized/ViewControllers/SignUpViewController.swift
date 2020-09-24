f6f96be0d5a357007e894ebe3c064feb3d37e69b
Robinson Paz <robinson.paz.jesus@gmail.com>
2020-09-24 15:39:46 -0600

    add create account screen

--- HealthyLivingOptimized/HealthyLivingOptimized/ViewControllers/SignUpViewController.swift
+++ HealthyLivingOptimized/HealthyLivingOptimized/ViewControllers/SignUpViewController.swift
@@ -8,89 +8,159 @@
 
 import UIKit
 
-class SignUpViewController: ScrollViewController {
-    
-    
-    // MARK: - Properties
-//    let signupForm = SignUpForm()
+import UIKit
+import Eureka
+//import ViewRow
+
+class SignUpViewController: FormViewController {
     
     
     // MARK: - UI Properties
-//    let bottomView = BottomButtonView()
-    
+//    let editPhotoView = EditPhotoView(frame: CGRect(x: 0, y: 0, width: 100, height: 150))
     
     
     // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
-        bind()
     }
     
     private func setupViews() {
-        view.backgroundColor = Assets.Color.pages
         title = "Create Account"
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.handleDismiss))
+        view.backgroundColor = Assets.Color.pages
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleDismiss))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(didTapSave))
         
-        ///
-//        contentView.addSubview(signupForm)
-//        signupForm.anchor(top: contentView.safeAreaLayoutGuide.topAnchor, left: contentView.leftAnchor, bottom: contentView.bottomAnchor, right: contentView.rightAnchor, tCons: 0, lCons: 16, bCons: 0, rCons: 16)
-//
-//        /// Bottom view
-//        bottomView.applyButton.setTitle("Sign Up", for: .normal)
-//        bottomView.applyButton.backgroundColor = Assets.Color.darkText
-//        bottomView.applyButton.tintColor = Assets.Color.cellWhite
-//        view.addSubview(bottomView)
-//        bottomView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, heightCons: 100)
+//        let profile = LocalDatabase.shared.getUser()
+        
+        
+//        editPhotoView.avatarView.set(with: profile?.imageUrl, placeholder: nil)
+//        editPhotoView.delegate = self
+        
+        /// Form
+        form
+            
+            /// Info
+            +++ Section(header: "Info", footer: nil)
+            <<< NameRow(){ row in
+                row.title = "First Name"
+                row.placeholder = ""
+            }
+            <<< NameRow(){ row in
+                row.title = "Last Name"
+                row.placeholder = ""
+            }
+            <<< EmailRow() { row in
+                row.title = "Email"
+            }
+            
+            /// Current Stats
+            +++ Section(header: "Current Stats", footer: "")
+            <<< DecimalRow { row in
+                row.title = "Weight"
+            }
+            <<< DecimalRow { row in
+                row.title = "Age"
+            }
+            <<< PickerRow<String> { row in
+                row.title = "Gender"
+                row.options = ["Male", "Female", "Other"]
+                
+            }
     }
     
-    private func bind() {
-//        bottomView.applyButton.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
-//        signupForm.delegate = self
-    }
-    
-    
-    // MARK: - Helper functions
-    @objc private func didTapSignUp() {
-        
-//        guard signupForm.isFormCompleted() else {
-//            print("Missing something")
-//            return
-//        }
-//
-//        let loader = animateLoader()
-//
-//        let authNetwork = AuthNetwork()
-//        authNetwork.signUp(name: signupForm.name,
-//                                     career: signupForm.career,
-//                                     internship: signupForm.internshipCheck,
-//                                     summerJob: signupForm.summerJobCheck,
-//                                     entryJob: signupForm.entryLevelCheck,
-//                                     email: signupForm.email,
-//                                     password: signupForm.password,
-//                                     userType: signupForm.type == .student ? "IND" : "COMP")
-//        { [weak self] (success, message) in
-//            loader.stopAnimating()
-//
-//            if success { /// SignUp and SignIn succeeded
-//                let codeViewController = CodeVerifyViewController.createAreference()
-//                codeViewController.email = self?.signupForm.email
-//                codeViewController.authNetwork = authNetwork
-//                self?.navigationController?.pushViewController(codeViewController, animated: true)
-//            } else {
-//                self?.presentError(message: message)
-//            }
-//        }
+    @objc private func didTapSave() {
+        /// Update Photo
     }
 }
 
 
-// MARK: - SignUpFormDelegate
-//extension SignUpViewController: SignUpFormDelegate {
+
+
+//class SignUpViewController: ScrollViewController {
 //
-//    func formDidChange(to type: SignUpForm.SignUpFormType) {
+//
+//    // MARK: - Properties
+////    let signupForm = SignUpForm()
 //
 //
+//    // MARK: - UI Properties
+////    let bottomView = BottomButtonView()
+//
+//
+//
+//    // MARK: - Init
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        setupViews()
+//        bind()
+//    }
+//
+//    private func setupViews() {
+//        view.backgroundColor = Assets.Color.pages
+//        title = "Create Account"
+//        navigationController?.navigationBar.prefersLargeTitles = true
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.handleDismiss))
+//
+//        ///
+////        contentView.addSubview(signupForm)
+////        signupForm.anchor(top: contentView.safeAreaLayoutGuide.topAnchor, left: contentView.leftAnchor, bottom: contentView.bottomAnchor, right: contentView.rightAnchor, tCons: 0, lCons: 16, bCons: 0, rCons: 16)
+////
+////        /// Bottom view
+////        bottomView.applyButton.setTitle("Sign Up", for: .normal)
+////        bottomView.applyButton.backgroundColor = Assets.Color.darkText
+////        bottomView.applyButton.tintColor = Assets.Color.cellWhite
+////        view.addSubview(bottomView)
+////        bottomView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, heightCons: 100)
+//    }
+//
+//    private func bind() {
+////        bottomView.applyButton.addTarget(self, action: #selector(didTapSignUp), for: .touchUpInside)
+////        signupForm.delegate = self
+//    }
+//
+//
+//    // MARK: - Helper functions
+//    @objc private func didTapSignUp() {
+//
+////        guard signupForm.isFormCompleted() else {
+////            print("Missing something")
+////            return
+////        }
+////
+////        let loader = animateLoader()
+////
+////        let authNetwork = AuthNetwork()
+////        authNetwork.signUp(name: signupForm.name,
+////                                     career: signupForm.career,
+////                                     internship: signupForm.internshipCheck,
+////                                     summerJob: signupForm.summerJobCheck,
+////                                     entryJob: signupForm.entryLevelCheck,
+////                                     email: signupForm.email,
+////                                     password: signupForm.password,
+////                                     userType: signupForm.type == .student ? "IND" : "COMP")
+////        { [weak self] (success, message) in
+////            loader.stopAnimating()
+////
+////            if success { /// SignUp and SignIn succeeded
+////                let codeViewController = CodeVerifyViewController.createAreference()
+////                codeViewController.email = self?.signupForm.email
+////                codeViewController.authNetwork = authNetwork
+////                self?.navigationController?.pushViewController(codeViewController, animated: true)
+////            } else {
+////                self?.presentError(message: message)
+////            }
+////        }
 //    }
 //}
+//
+//
+//// MARK: - SignUpFormDelegate
+////extension SignUpViewController: SignUpFormDelegate {
+////
+////    func formDidChange(to type: SignUpForm.SignUpFormType) {
+////
+////
+////    }
+////}
